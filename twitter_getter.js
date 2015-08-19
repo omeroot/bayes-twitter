6a6291c9bd27d4bc2ab097f25a8b0569e103d426
Markakod Development <markakoddevelopment@Markakods-iMac.local>
2015-08-19 11:28:51 +0300

    callback

--- twitter_getter.js
+++ twitter_getter.js
@@ -3,118 +3,128 @@ var http = require('http');
 var fs = require('fs');
 var conf = require('./config')
 
-var download = function(url, dest, cb) {
-  var file = fs.createWriteStream(dest);
-  http.get(url, function(response) {
-    response.pipe(file);
-    file.on('finish', function() {
-      file.close(cb);  // close() is async, call cb after close completes.
-    });
-  }).on('error', function(err) { 
-    fs.unlink(dest); 
-    if (cb) cb(err.message);
-  });
+var download = function (url, dest, cb) {
+	var file = fs.createWriteStream(dest);
+	http.get(url, function (response) {
+		response.pipe(file);
+		file.on('finish', function () {
+			file.close(cb);  // close() is async, call cb after close completes.
+		});
+	}).on('error', function (err) {
+		fs.unlink(dest);
+		if (cb) cb(err.message);
+	});
 };
 
 var client = new Twit({
-	consumer_key : conf.consumer_key,
-	consumer_secret : conf.consumer_secret,
-	access_token : conf.access_token,
-	access_token_secret : conf.access_token_secret
+	consumer_key: conf.consumer_key,
+	consumer_secret: conf.consumer_secret,
+	access_token: conf.access_token,
+	access_token_secret: conf.access_token_secret
 });
 
-var fetchImages = function(user){
+var fetchImages = function (user) {
 	client.get("statuses/user_timeline", {
 		screen_name: user,
 		count: 40
 	}, function (err, data, response) {
-		data.forEach(function(element,index,array){
+		data.forEach(function (element, index, array) {
 			var m = element.entities.media
-			if (typeof m != 'undefined'){
+			if (typeof m != 'undefined') {
 				var url = element.entities.media[0].media_url;
-				download(url, index + ".jpg",function(err){
-					if(err) throw err
+				download(url, index + ".jpg", function (err) {
+					if (err) throw err
 				});
 			}
 		});
 	});
-}
+};
 
-var fetchTweets = function(user,count, callback){
+var fetchTweets = function (user, count, callback) {
 	var conf = {
-		screen_name : user,
-		count : count
+		screen_name: user,
+		count: count
 	}
-	var tweets = [ ]
+	var tweets = []
 	client.get("statuses/user_timeline", conf, function (err, data, response) {
-		for (var i in data){
+		for (var i in data) {
 			tweets.push(data[i].text)
 		}
-		
-		callback(tweets);
+		//if (tweets.length == 0) return callback(new Error('timeline is empty!' + " for " + user))
+		callback(null, tweets);
 	});
-}
+};
 
-var clearMentionAndUrl = function(data){
+var clearMentionAndUrl = function (data, callback) {
 	var words = []
-	
-	for (var i in data){
+
+	for (var i in data) {
 		var counter = 0
 		var arr = data[i].split(" ")
-		for (var j in arr){
+		for (var j in arr) {
 			var subArr = arr[j].split("")
-			if (subArr[0] == '@' || isUrl( subArr )){
+			if (subArr[0] == '@' || isUrl(subArr)) {
 				counter = 1
 				break
 			}
 		}
-		if ( counter == 0)
+		if (counter == 0)
 			words.push(data[i])
 	}
-	return words	
-}
+	return callback(null, words)
+};
 
-var isUrl = function(word){
+var isUrl = function (word) {
 	var first4 = [];
-	for (var j in word){
+	for (var j in word) {
 		first4.push(word[j])
-		if(j == 3)
+		if (j == 3)
 			break
 	}
-	if( first4.join("") == "http")
+	if (first4.join("") == "http")
 		return true
 	else
 		return false
-}
+};
 
-var splitSubData = function(data,callback){
-	var arr = data.replace(/\n/g," ").split(" ")
-	callback(null,arr)
-}
+var splitSubData = function (data, callback) {
+	var arr = data.replace(/\n/g, " ").split(" ")
+	callback(null, arr)
+};
 
-var splitData = function(data,callback){
+var splitData = function (data, callback) {
 	var d = []
 	var counter = 0
 	if (data.length == 0)
 		callback(new Error('data is empty'))
-		
-	data.forEach(function(element,index,array){
-		splitSubData(element,function(err,data){
+
+	data.forEach(function (element, index, array) {
+		splitSubData(element, function (err, data) {
 			d.push(data)
 			counter += 1
 		});
 		if (counter == data.length)
-			callback(null,d)
+			callback(null, d)
 	});
-}
+};
 
-fetchTweets('semtinmayoru',400, function (l) {
-	var f = fs.createWriteStream(new Date().getTime()+".txt")
-	var clear = clearMentionAndUrl(l)
-	console.log(clear.length)
-	for( var i in clear ){
-		f.write(clear[i] + '\n')
-	}
-	f.end()
-	
-});
+var dump = function () {
+	var list = ["rabula_", "semtinmayoru", "fenasi_", "kontravolta_"]
+	list.forEach(function (element) {
+		console.log(element, "fetching...")
+		fetchTweets(element, 400, function (err, l) {
+			if (err) throw err
+			clearMentionAndUrl(l, function (err, data) {
+				var f = fs.createWriteStream("./dataset/" + new Date().getTime() + ".txt")
+				console.log(data.length,element)
+				for (var i in data) {
+					f.write(data[i] + '\n')
+				}
+				f.end()
+			})
+
+		});
+	});
+};
+
+dump();
